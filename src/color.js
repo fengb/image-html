89941bba32df2627dbcd888797d40669a6bd0eb7
fengb <contact@fengb.info>
2013-07-05 21:01:11 -0500

    Added Color#hex()

--- src/color.js
+++ src/color.js
@@ -1,17 +1,24 @@
-var Color = {
-  rgb: function(r, g, b){
-    return {
-      r: r,
-      g: g,
-      b: b
-    };
-  },
+function Color(r, g, b){
+  this.r = r;
+  this.g = g;
+  this.b = b;
+}
 
-  hex: function(raw){
-    return {
-      r: parseInt(raw.substring(0, 2), 16),
-      g: parseInt(raw.substring(2, 4), 16),
-      b: parseInt(raw.substring(4, 6), 16)
-    };
+Color.prototype.hex = function(){
+  function hexify(num){
+    var ret = num.toString(16);
+    return ret.length == 1 ? '0' + ret : ret;
   }
+
+  return hexify(this.r) + hexify(this.g) + hexify(this.b);
+};
+
+Color.rgb = function(r, g, b){
+  return new Color(r, g, b);
+};
+
+Color.hex = function(hex){
+  return new Color(parseInt(hex.substring(0, 2), 16),
+                   parseInt(hex.substring(2, 4), 16),
+                   parseInt(hex.substring(4, 6), 16));
 };
--- test/color_test.js
+++ test/color_test.js
@@ -1,10 +1,23 @@
 describe('Color', function(){
+  describe('inverse', function(){
+    it('is hex', function(){
+      expect(Color.hex('123456').hex()).to.equal('123456');
+    });
+  });
+
   describe('.rgb()', function(){
+    before(function(){
+      this.color = Color.rgb(1, 2, 3);
+    });
+
     it('has correct rgb values', function(){
-      var color = Color.rgb(1, 2, 3);
-      expect(color.r).to.equal(1);
-      expect(color.g).to.equal(2);
-      expect(color.b).to.equal(3);
+      expect(this.color.r).to.equal(1);
+      expect(this.color.g).to.equal(2);
+      expect(this.color.b).to.equal(3);
+    });
+
+    it('has the correct hex value', function(){
+      expect(this.color.hex()).to.equal('010203');
     });
   });
 
@@ -13,13 +26,6 @@ describe('Color', function(){
       var color = Color.hex('010203');
       var rgb = Color.rgb(1, 2, 3);
       expect(color).to.eql(rgb);
-    });
-
-    it('has correct rgb values', function(){
-      var color = Color.hex('010203');
-      expect(color.r).to.equal(1);
-      expect(color.g).to.equal(2);
-      expect(color.b).to.equal(3);
     });
   });
 });
