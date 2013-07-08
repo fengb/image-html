4d2b7b567f00eb88b56abf8044f702fe7ee03782
fengb <contact@fengb.info>
2013-07-08 09:27:20 -0500

    Fixed image generation

--- src/image_html.js
+++ src/image_html.js
@@ -2,15 +2,15 @@ var ImageHtml = {
   run: function(pixels, id){
     return {
       css: function(){
-        return format('#{0} { border-collapse: collapse; }' +
-                      '#{0} * { width: 1px; height: 1px; }', id);
+        return format('#{0} { border-spacing: 0; }' +
+                      '#{0} tr { height: 1px; }', id);
       },
 
       html: function(){
         var ret = format('<table id="{0}">', id);
-        for(var x=0; x < pixels.width; x++){
+        for(var y=0; y < pixels.height; y++){
           ret += '<tr>';
-          for(var y=0; y < pixels.height; y++){
+          for(var x=0; x < pixels.width; x++){
             ret += format('<td style="background: #{0}"></td>', pixels.get(x, y).hex());
           }
           ret += '</tr>';
