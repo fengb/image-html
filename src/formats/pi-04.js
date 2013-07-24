f99dde03cef64f3c8298316eea69f4562f57be07
fengb <contact@fengb.info>
2013-07-24 01:32:46 -0500

    Converted HtmlGenerator to be non-prototype based

--- src/formats/pi-04.js
+++ src/formats/pi-04.js
@@ -38,17 +38,7 @@ module.exports = function(pixels, id){
 };
 
 
-function stringifyStyles(styleDict){
-  var stringified = [];
-  for(var key in styleDict){
-    stringified.push(util.format('{0}: {1}', key, styleDict[key]));
-  }
-  return stringified;
-}
-
-
-var HtmlGenerator = module.exports.HtmlGenerator = function(styleDicts){
-  this._styleDicts = styleDicts;
+module.exports.HtmlGenerator = function(styleDicts){
   var flatStyles = [];
   styleDicts.forEach(function(styleDict){
     flatStyles.push.apply(flatStyles, stringifyStyles(styleDict));
@@ -58,70 +48,82 @@ var HtmlGenerator = module.exports.HtmlGenerator = function(styleDicts){
   var generator = util.generator('abcdefghijklmnopqrstuvwxyz');
   var sortedCounter = counter.sortByMostFrequent();
 
-  this._classes = {};
+  var privClasses = {};
   for(var i=0; i < sortedCounter.length; i++){
     var key = sortedCounter[i][0];
     var val = sortedCounter[i][1];
     if(val > 1){
-      this._classes[key] = generator();
-    }
-  }
-};
-
-HtmlGenerator.prototype.commonStyles = function(attr){
-  var counter = util.counter();
-  for(var i=0; i < this._styleDicts.length; i++){
-    if(this._styleDicts[i][attr]){
-      counter.add(this._styleDicts[i][attr]);
-    }
-  }
-  var sortedCounter = counter.sortByMostFrequent();
-  var ret = [];
-  for(i=0; i < sortedCounter.length; i++){
-    if(sortedCounter[i][1] > 1){
-      ret.push(sortedCounter[i][0]);
-    }
-  }
-  return ret;
-};
-
-HtmlGenerator.prototype.styles = function(prepend){
-  var ret = [util.format('{0} * { display: inline-block; }', prepend)];
-  for(var c in this._classes){
-    ret.push(util.format('{0} .{1} { {2} }', prepend, this._classes[c], c));
-  }
-  return ret;
-};
-
-HtmlGenerator.prototype.valuesFor = function(searchStyles){
-  searchStyles = stringifyStyles(searchStyles);
-
-  var classes = [];
-  var styles = [];
-  for(var i=0; i < searchStyles.length; i++){
-    var style = searchStyles[i];
-    if(this._classes[style]){
-      classes.push(this._classes[style]);
-    } else {
-      styles.push(style);
+      privClasses[key] = generator();
     }
   }
 
-  return {
-    element: 'i',
-    classes: classes.join(' '),
-    styles: styles.join('; ')
+  function stringifyStyles(styleDict){
+    var stringified = [];
+    for(var key in styleDict){
+      stringified.push(util.format('{0}: {1}', key, styleDict[key]));
+    }
+    return stringified;
+  }
+
+  var public = {
+    commonStyles: function(attr){
+      var counter = util.counter();
+      for(var i=0; i < styleDicts.length; i++){
+        if(styleDicts[i][attr]){
+          counter.add(styleDicts[i][attr]);
+        }
+      }
+      var sortedCounter = counter.sortByMostFrequent();
+      var ret = [];
+      for(i=0; i < sortedCounter.length; i++){
+        if(sortedCounter[i][1] > 1){
+          ret.push(sortedCounter[i][0]);
+        }
+      }
+      return ret;
+    },
+
+    styles: function(prepend){
+      var ret = [util.format('{0} * { display: inline-block; }', prepend)];
+      for(var c in privClasses){
+        ret.push(util.format('{0} .{1} { {2} }', prepend, privClasses[c], c));
+      }
+      return ret;
+    },
+
+    valuesFor: function(searchStyles){
+      searchStyles = stringifyStyles(searchStyles);
+
+      var classes = [];
+      var styles = [];
+      for(var i=0; i < searchStyles.length; i++){
+        var style = searchStyles[i];
+        if(privClasses[style]){
+          classes.push(privClasses[style]);
+        } else {
+          styles.push(style);
+        }
+      }
+
+      return {
+        element: 'i',
+        classes: classes.join(' '),
+        styles: styles.join('; ')
+      };
+    },
+
+    elementFor: function(searchStyles){
+      var values = public.valuesFor(searchStyles);
+      var attrs = '';
+      if(values.classes){
+        attrs += util.format(' class="{0}"', values.classes);
+      }
+      if(values.styles){
+        attrs += util.format(' style="{0}"', values.styles);
+      }
+      return util.format('<{0}{1}></{0}\n>', values.element, attrs);
+    }
   };
+  return public;
 };
 
-HtmlGenerator.prototype.elementFor = function(searchStyles){
-  var values = this.valuesFor(searchStyles);
-  var attrs = '';
-  if(values.classes){
-    attrs += util.format(' class="{0}"', values.classes);
-  }
-  if(values.styles){
-    attrs += util.format(' style="{0}"', values.styles);
-  }
-  return util.format('<{0}{1}></{0}\n>', values.element, attrs);
-};
