9b98098ea5d31701a8155bdb7d5c80e1eb623043
fengb <contact@fengb.info>
2013-07-26 17:44:06 -0500

    v0.3.0

--- image-html.js
+++ image-html.js
@@ -1,4 +1,4 @@
-/*! image-html v0.2.0 - https://github.com/fengb/image-html */
+/*! image-html v0.3.0 - https://github.com/fengb/image-html */
 ;(function(e,t,n){function i(n,s){if(!t[n]){if(!e[n]){var o=typeof require=="function"&&require;if(!s&&o)return o(n,!0);if(r)return r(n,!0);throw new Error("Cannot find module '"+n+"'")}var u=t[n]={exports:{}};e[n][0].call(u.exports,function(t){var r=e[n][1][t];return i(r?r:t)},u,u.exports)}return t[n].exports}var r=typeof require=="function"&&require;for(var s=0;s<n.length;s++)i(n[s]);return i})({1:[function(require,module,exports){
 window.ImageHtml = require('./src/image-html');
 
@@ -49,7 +49,101 @@ Color.hex = function(hex){
 };
 
 },{}],4:[function(require,module,exports){
-var util = require('./util');
+var format = require('../util.js').format;
+
+
+module.exports = function(pixels, id){
+  return {
+    css: function(){
+      return format('#{0} { width: {1}px; margin: 0; overflow: hidden }\n' +
+                    '#{0} i { float: left; width: 1px; height: 1px; }',
+                    id, pixels.cols);
+    },
+
+    html: function(){
+      var ret = format('<p id="{0}"\n>', id);
+      for(var row=0; row < pixels.rows; row++){
+        for(var col=0; col < pixels.cols; col++){
+          ret += format('<i style="background: #{0}"></i\n>', pixels[row][col].hex());
+        }
+      }
+      return ret + '</p>';
+    }
+  };
+};
+
+},{"../util.js":11}],5:[function(require,module,exports){
+var format = require('../util.js').format;
+var PixelSegments = require('../pixel-segments.js');
+
+
+module.exports = function(pixels, id){
+  var pixelSegments = PixelSegments(pixels);
+
+  return {
+    css: function(){
+      return format('#{0} { width: {1}px; margin: 0; overflow: hidden }\n' +
+                    '#{0} i { float: left; width: 1px; height: 1px; }',
+                    id, pixels.cols);
+    },
+
+    html: function(){
+      var ret = format('<p id="{0}"\n>', id);
+      for(var row=0; row < pixelSegments.length; row++){
+        for(var segmentNum=0; segmentNum < pixelSegments[row].length; segmentNum++){
+          var segment = pixelSegments[row][segmentNum];
+          if(segment.length > 1){
+            ret += format('<i style="background: #{0}; width: {1}px"></i\n>',
+                          segment.value.hex(), segment.length);
+          } else {
+            ret += format('<i style="background: #{0}"></i\n>', segment.value.hex());
+          }
+        }
+      }
+      return ret + '</p>';
+    }
+  };
+};
+
+},{"../pixel-segments.js":9,"../util.js":11}],6:[function(require,module,exports){
+var util = require('../util');
+var PixelSegments = require('../pixel-segments.js');
+
+
+module.exports = function(pixels, id){
+  var pixelSegments = PixelSegments(pixels);
+  var unrolledSegments = [];
+  for(var row=0; row < pixelSegments.length; row++){
+    unrolledSegments.push.apply(unrolledSegments, pixelSegments[row]);
+  }
+
+  var styles = [];
+  for(var i=0; i < unrolledSegments.length; i++){
+    var segment = unrolledSegments[i];
+    segment.styles = {background: '#' + segment.value.hex(),
+                      width: segment.length + 'px'};
+    styles.push(segment.styles);
+  }
+  var classer = new CssClasser(styles);
+
+  return {
+    css: function(){
+      return util.format('#{0} { width: {1}px; margin: 0; overflow: hidden; }\n' +
+                         '#{0} i { float: left; height: 1px; }\n',
+                         id, pixels.cols) +
+             classer.classes('#'+id).join('\n');
+    },
+
+    html: function(){
+      var ret = util.format('<p id="{0}"\n>', id);
+      for(var i=0; i < unrolledSegments.length; i++){
+        var segment = unrolledSegments[i];
+        ret += util.format('<i {0}></i\n>', classer.attrsFor(segment.styles));
+      }
+      return ret + '</p>';
+    }
+  };
+};
 
 
 function stringifyStyles(styleDict){
@@ -61,18 +155,15 @@ function stringifyStyles(styleDict){
 }
 
 
-var CssClasser = module.exports = function(styleDicts){
+var CssClasser = module.exports.CssClasser = function(styleDicts){
   var flatStyles = [];
   styleDicts.forEach(function(styleDict){
     flatStyles.push.apply(flatStyles, stringifyStyles(styleDict));
   });
 
   var counter = util.counter(flatStyles);
-  var generator = util.generator('abcdefghijklmnopqrstuvwxyz');
-  var sortedCounter = util.objToArray(counter).sort(function(a, b){
-    return a[1] < b[1] ?  1 :
-           a[1] > b[1] ? -1 : 0;
-  });
+  var generator = util.endlessGenerator('abcdefghijklmnopqrstuvwxyz');
+  var sortedCounter = counter.sortByMostFrequent();
 
   this._classes = {};
   for(var i=0; i < sortedCounter.length; i++){
@@ -116,67 +207,9 @@ CssClasser.prototype.attrsFor = function(searchStyles){
   return ret.join(' ');
 };
 
-},{"./util":11}],5:[function(require,module,exports){
-var format = require('../util.js').format;
-
-
-module.exports = function(pixels, id){
-  return {
-    css: function(){
-      return format('#{0} { width: {1}px; margin: 0; }\n' +
-                    '#{0} i { display: inline-block; width: 1px; height: 1px; }',
-                    id, pixels.cols);
-    },
-
-    html: function(){
-      var ret = format('<p id="{0}"\n>', id);
-      for(var row=0; row < pixels.rows; row++){
-        for(var col=0; col < pixels.cols; col++){
-          ret += format('<i style="background: #{0}"></i\n>', pixels[row][col].hex());
-        }
-      }
-      return ret + '</p>';
-    }
-  };
-};
-
-},{"../util.js":11}],6:[function(require,module,exports){
-var format = require('../util.js').format;
+},{"../pixel-segments.js":9,"../util":11}],7:[function(require,module,exports){
+var util = require('../util');
 var PixelSegments = require('../pixel-segments.js');
-
-
-module.exports = function(pixels, id){
-  var pixelSegments = PixelSegments(pixels);
-
-  return {
-    css: function(){
-      return format('#{0} { width: {1}px; margin: 0; }\n' +
-                    '#{0} i { display: inline-block; width: 1px; height: 1px; }',
-                    id, pixels.cols);
-    },
-
-    html: function(){
-      var ret = format('<p id="{0}"\n>', id);
-      for(var row=0; row < pixelSegments.length; row++){
-        for(var segmentNum=0; segmentNum < pixelSegments[row].length; segmentNum++){
-          var segment = pixelSegments[row][segmentNum];
-          if(segment.length > 1){
-            ret += format('<i style="background: #{0}; width: {1}px"></i\n>',
-                          segment.value.hex(), segment.length);
-          } else {
-            ret += format('<i style="background: #{0}"></i\n>', segment.value.hex());
-          }
-        }
-      }
-      return ret + '</p>';
-    }
-  };
-};
-
-},{"../pixel-segments.js":9,"../util.js":11}],7:[function(require,module,exports){
-var format = require('../util.js').format;
-var PixelSegments = require('../pixel-segments.js');
-var CssClasser = require('../css-classer.js');
 
 
 module.exports = function(pixels, id){
@@ -189,37 +222,192 @@ module.exports = function(pixels, id){
   var styles = [];
   for(var i=0; i < unrolledSegments.length; i++){
     var segment = unrolledSegments[i];
-    segment.styles = {background: '#' + segment.value.hex(),
-                      width: segment.length + 'px'};
+    segment.styles = [
+      util.format('background: #{0}', segment.value.hex()),
+      util.format('width: {0}px', segment.length)
+    ];
     styles.push(segment.styles);
   }
-  var classer = new CssClasser(styles);
+  var fmt = Formatter.fromStylesElements(styles);
 
   return {
     css: function(){
-      return format('#{0} { width: {1}px; margin: 0; }\n' +
-                    '#{0} i { display: inline-block; width: 1px; height: 1px; }\n',
-                    id, pixels.cols) +
-             classer.classes('#'+id).join('\n');
+      return util.format('#{0} { width: {1}px; margin: 0; overflow: hidden }\n'+
+                         '#{0} * { float: left; height: 1px; vertical-align: baseline; margin: 0; padding: 0; border-collapse: collapse }\n'+
+                         '#{0} *:before { content: "" }\n'+
+                         '#{0} *:after { content: "" }\n',
+                         id, pixels.cols) +
+             fmt.styles('#'+id).join('\n');
     },
 
     html: function(){
-      var ret = format('<p id="{0}"\n>', id);
-      for(var i=0; i < unrolledSegments.length; i++){
-        var segment = unrolledSegments[i];
-        ret += format('<i {0}></i\n>', classer.attrsFor(segment.styles));
-      }
-      return ret + '</p>';
+      return util.format('<div id="{0}"\n>{1}</div>', id,
+                         unrolledSegments.map(function(s){
+                           return fmt.elementFor(s.styles);
+                         }).join(''));
     }
   };
 };
 
-},{"../css-classer.js":4,"../pixel-segments.js":9,"../util.js":11}],8:[function(require,module,exports){
+
+function bestMatch(tags, stylesElement){
+  for(var tag in tags){
+    if(util.arrayEquals(tags[tag], stylesElement)){
+      return {
+        tag: tag,
+        unmatchedStyles: []
+      };
+    }
+  }
+
+  /* Tag matches one thing */
+  for(var t in tags){
+    for(var i=0; i < stylesElement.length; i++){
+      var tagStyles = tags[t];
+      if(t.length < 3 && tags[t].indexOf(stylesElement[i]) != -1){
+        return {
+          tag: t,
+          unmatchedStyles: util.arrayDiff(stylesElement, tagStyles || [])
+        };
+      }
+    }
+  }
+  return {
+    tag: null,
+    unmatchedStyles: stylesElement
+  };
+}
+
+
+var Aggregator = module.exports.Aggregator = function(stylesElements){
+  var tags = function(){
+    var counter = util.counter();
+    stylesElements.forEach(function(stylesElement){
+      counter.add(stylesElement.join(';'));
+    });
+
+    var sortedCounter = counter.sortByMostFrequent();
+    var generator = util.generator([
+      'i', 'b', 'a', 'p', 'q', 'u', 's',
+      'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'ol', 'ul', 'dl',
+      'dfn', 'kbd', 'var', 'ins', 'del', 'sup', 'sub', 'nav', 'pre', 'div', 'bdi',
+      'span', 'samp', 'code', 'abbr', 'form', 'cite', 'mark', 'menu',
+      'small', 'aside', 'table'
+    ]);
+    var tags = {};
+    for(var i=0; i < sortedCounter.length; i++){
+      var key = sortedCounter[i][0];
+      var val = sortedCounter[i][1];
+      var nextTag = generator();
+      if(val <= 1 || nextTag === null){
+        break;
+      }
+      tags[nextTag] = key.split(';');
+    }
+    return tags;
+  }();
+
+  return {
+    tags: tags,
+    classes: function(){
+      var counter = util.counter();
+      stylesElements.forEach(function(stylesElement){
+        var unmatchedStyles = bestMatch(tags, stylesElement).unmatchedStyles;
+        for(var i=0; i < unmatchedStyles.length; i++){
+          counter.add(unmatchedStyles[i]);
+        }
+      });
+
+      var generator = util.endlessGenerator('abcdefghijklmnopqrstuvwxyz');
+      var sortedCounter = counter.sortByMostFrequent();
+
+      var classes = {};
+      for(var i=0; i < sortedCounter.length; i++){
+        var key = sortedCounter[i][0];
+        var val = sortedCounter[i][1];
+        if(val <= 1){
+          break;
+        }
+        classes[key] = generator();
+      }
+      return classes;
+    }()
+  };
+};
+
+var Formatter = module.exports.Formatter = function(aggregate){
+  var self = {
+    defaultTag: function(){
+      var tag;
+      for(var t in aggregate.tags){
+        if(!tag || t.length < tag.length){
+          tag = t;
+        }
+      }
+      return tag;
+    }(),
+
+    styles: function(prepend){
+      var ret = [];
+      for(var t in aggregate.tags){
+        ret.push(util.format('{0} {1} { {2} }', prepend, t, aggregate.tags[t].join('; ')));
+      }
+
+      for(var c in aggregate.classes){
+        ret.push(util.format('{0} .{1} { {2} }', prepend, aggregate.classes[c], c));
+      }
+      return ret;
+    },
+
+    valuesFor: function(searchStyles){
+      var match = bestMatch(aggregate.tags, searchStyles);
+      var tag = match.tag || self.defaultTag;
+
+      var classes = [];
+      var styles = [];
+      for(var i=0; i < match.unmatchedStyles.length; i++){
+        var style = match.unmatchedStyles[i];
+        if(aggregate.classes[style]){
+          classes.push(aggregate.classes[style]);
+        } else {
+          styles.push(style);
+        }
+      }
+
+      return {
+        tag: tag,
+        classes: classes.join(' '),
+        styles: styles.join('; ')
+      };
+    },
+
+    elementFor: function(searchStyles){
+      var values = self.valuesFor(searchStyles);
+      var attrs = '';
+      if(values.classes){
+        attrs += util.format(' class="{0}"', values.classes);
+      }
+      if(values.styles){
+        attrs += util.format(' style="{0}"', values.styles);
+      }
+      return util.format('<{0}{1}></{0}\n>', values.tag, attrs);
+    }
+  };
+  return self;
+};
+
+Formatter.fromStylesElements = function(stylesElements){
+  var agg = Aggregator(stylesElements);
+  return Formatter(agg);
+};
+
+},{"../pixel-segments.js":9,"../util":11}],8:[function(require,module,exports){
 var Pixels = require('./pixels');
 var formats = {
-  'pi-01': require('./formats/pi-01'),
-  'pi-02': require('./formats/pi-02'),
-  'pi-03': require('./formats/pi-03')
+  '01': require('./formats/01'),
+  '02': require('./formats/02'),
+  '03': require('./formats/03'),
+  '04': require('./formats/04')
 };
 
 var ImageHtml = module.exports = {
@@ -228,13 +416,13 @@ var ImageHtml = module.exports = {
   },
 
   convert: function(image, id, format){
-    format = format || 'pi-03';
+    format = format || '04';
     var pixels = Pixels.fromImage(image);
     return formats[format](pixels, id);
   }
 };
 
-},{"./formats/pi-01":5,"./formats/pi-02":6,"./formats/pi-03":7,"./pixels":10}],9:[function(require,module,exports){
+},{"./formats/01":4,"./formats/02":5,"./formats/03":6,"./formats/04":7,"./pixels":10}],9:[function(require,module,exports){
 module.exports = function(pixels){
   var rows = [];
   for(var row=0; row < pixels.rows; row++){
@@ -288,7 +476,7 @@ Pixels.fromImage = function(domImage){
 };
 
 },{"./color":3,"canvas-browserify":2}],11:[function(require,module,exports){
-module.exports = {
+var util = module.exports = {
   format: function() {
     var regexes = [];
     for(var i=0; i < 10; i++) {
@@ -306,12 +494,26 @@ module.exports = {
   }(),
 
   counter: function(array){
-    var ret = {};
-    for(var i=0; i < array.length; i++){
-      var val = array[i];
-      ret[val] = (ret[val] || 0) + 1;
+    var count = {};
+    function add(value){
+      count[value] = (count[value] || 0) + 1;
     }
-    return ret;
+    if(array){
+      for(var i=0; i < array.length; i++){
+        add(array[i]);
+      }
+    }
+
+    return {
+      count: count,
+      add: add,
+      sortByMostFrequent: function(){
+        return util.objToArray(this.count).sort(function(a, b){
+          return a[1] < b[1] ?  1 :
+                 a[1] > b[1] ? -1 : 0;
+        });
+      }
+    };
   },
 
   objToArray: function(obj){
@@ -322,7 +524,50 @@ module.exports = {
     return array;
   },
 
+  memoize: function(func){
+    var cache = {};
+    return function(){
+      var key = [].join.apply(arguments);
+      if(!cache[key]){
+        cache[key] = func.apply(this, arguments);
+      }
+      return cache[key];
+    };
+  },
+
+  arrayEquals: function(arr1, arr2){
+    if(arr1 === arr2) return true;
+    if(!arr1 || !arr2) return false;
+    if(arr1.length != arr2.length) return false;
+
+    for(var i=0; i < arr2.length; ++i) {
+      if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+  },
+
+  arrayDiff: function(arr1, arr2){
+    var diff = [];
+    for(var i1=0; i1 < arr1.length; i1++){
+      var i2 = arr2.indexOf(arr1[i1]);
+      if(i2 === -1){
+        diff.push(arr1[i1]);
+      }
+    }
+    return diff;
+  },
+
   generator: function(chars){
+    var index = 0;
+    return function(){
+      if(index < chars.length){
+        return chars[index++];
+      }
+      return null;
+    };
+  },
+
+  endlessGenerator: function(chars){
     var indexes = [];
 
     return function(){
